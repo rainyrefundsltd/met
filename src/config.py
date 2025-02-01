bff25823ba11b4406e4443641ca630dd6d52ad3d
Alex Hall <alexander.hall@rainyrefunds.com>
2025-02-01 12:33:01 +0000

    Added s3 list_object_v2 functionality

--- src/config.py
+++ src/config.py
@@ -9,3 +9,18 @@ MET_OFFICE_API_KEY = os.getenv("MET_OFFICE_API_KEY")
 
 if not MET_OFFICE_API_KEY:
     raise ValueError("Missing MET_OFFICE_API_KEY. Please set it in the .env file.")
+
+AWS_ACCESS = os.getenv("AWS_ACCESS")
+
+if not MET_OFFICE_API_KEY:
+    raise ValueError("Missing AWS_ACCESS. Please set it in the .env file.")
+
+AWS_SECRET = os.getenv("AWS_SECRET")
+
+if not MET_OFFICE_API_KEY:
+    raise ValueError("Missing AWS_SECRET. Please set it in the .env file.")
+
+AWS_REGION = os.getenv("AWS_REGION")
+
+if not MET_OFFICE_API_KEY:
+    raise ValueError("Missing AWS_REGION. Please set it in the .env file.")
--- src/fetch_asdi.py
+++ src/fetch_asdi.py
@@ -1,12 +1,40 @@
 # Script to fetch ASDI data
 
-import os
+from aiohttp import ClientError
+import boto3
+import logging
+from config import AWS_ACCESS, AWS_SECRET, AWS_REGION
 
-data_folder = "data"
-file_path = os.path.join(data_folder, "example.csv")
+bucket_name = "met-office-atmospheric-model-data"
+prefix = "uk-deterministic-2km/"
 
-# Writing data
-with open(file_path, "w") as f:
-    f.write("Sample data")
 
-print(f"Data saved to {file_path}")
+def list_object_v2(aws_access, aws_secret, aws_region, bucket_name, prefix):
+    
+    """List objects in a specified AWS bucket, with a specified prefix"""
+    
+    s3_client = boto3.client(
+        's3',
+        aws_access_key_id = aws_access,
+        aws_secret_access_key=aws_secret,
+        region_name=aws_region
+    )
+    
+    try:
+        response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
+        
+        if "Contents" in response:
+            for content in response['Contents']:
+                print(f'    {content["Key"]}, {content["Size"]/1024} KB')
+        else:
+            print(f"No objects found in {prefix}")
+    
+    except ClientError as e:
+        logging.error(e)
+        return False
+    
+    return True
+
+if __name__ == "__main__":
+
+    list_object_v2(AWS_ACCESS, AWS_SECRET, AWS_REGION, bucket_name, prefix)
